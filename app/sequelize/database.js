b502498a8efabeacfcf1c1bfe48e79c3cf2b3037
Narayan Sharma <narayansharma@ucreate.co.in>
2019-04-16 17:49:00 +0530

    disabled logging while testing

--- app/sequelize/database.js
+++ app/sequelize/database.js
@@ -12,6 +12,7 @@ module.exports = {
     database: 'database_testing',
     host: '127.0.0.1',
     dialect: 'postgres',
+    logging: false,
   },
   production: {
     database: process.env.DATABASE_NAME,
