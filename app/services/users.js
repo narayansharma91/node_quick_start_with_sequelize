a7ecd424ba10527d555556cf738fd8d063738beb
Narayan Sharma <narayansharma@ucreate.co.in>
2019-04-17 16:09:42 +0530

    Thrown exception

--- app/services/users.js
+++ app/services/users.js
@@ -1,10 +1,18 @@
+const { UserNotFound } = require('../util/exceptions/errors');
+
 const userService = ({
   repos: {
     userRepo: { getUsers, getUserDetail, createUser },
   },
 }) => {
   const getUserService = () => getUsers();
-  const getUserDetailService = id => getUserDetail(id);
+  const getUserDetailService = async (id) => {
+    const userInfo = await getUserDetail(id);
+    if (!userInfo) {
+      throw new UserNotFound('User not found');
+    }
+    return userInfo;
+  };
   const createUserService = data => createUser(data);
   return {
     getUserService,
